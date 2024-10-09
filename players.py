90800d1073f2e95b547638f4bc34c0252fb74b46
connorrowland04 <157513832+connorrowland04@users.noreply.github.com>
2024-10-09 13:10:02 -0400

    Add files via upload

---
+++ players.py
@@ -0,0 +1,49 @@
+import random 
+
+# Driver: Connor Rowland
+# Navigator: Gokul Mahendran
+# Date: 10/02/2024 
+# Exercise 5 - Marathon 
+
+class Player:
+    def __init__(self,name,position = 0): 
+        self.name = name 
+        self.position = position 
+
+
+
+class RedPlayer(Player): 
+    def walk(self):
+        steps = random.randrange(1,11)
+        self.position += steps
+
+
+class BluePlayer(Player):
+    def walk(self):
+        steps = random.randrange(4,9)
+        self.position += steps
+
+
+def play_game():
+    player_list = []
+    x = 1 
+    while len(player_list) < 6: 
+        if x <= 3:
+            player_list.append(BluePlayer(f"BluePlayer{x}"))
+        else: 
+            player_list.append(RedPlayer(f"RedPlayer{x - 3}"))
+        x += 1
+    turns = 0 
+    while True:
+        turns += 1
+        for player in player_list:
+            player.walk()
+            if player.position >= 100:
+                return player.name,turns
+
+
+
+if __name__ == "__main__":
+    winner, turns = play_game() 
+    print(f"The Winner is... {winner} and it took {turns} turns")
+
